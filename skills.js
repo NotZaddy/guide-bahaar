34307341340bc1186986edb46bf7716945b8d46f
NotZaddy <45076913+NotZaddy@users.noreply.github.com>
2019-02-03 21:11:09 +0100

    Update skills.js

--- skills.js
+++ skills.js
@@ -1,39 +1,40 @@
 module.exports = {
 	BossActionsTips : {
-		101: {msg: '4 Hit combo'},//101 105 107 139
-		103: {msg: 'Front (Dodge)'},//103 104
-//		105: {msg: '4 Hit (2)'},
-//		107: {msg: '4 Hit (3)'},	
-		108: {msg: 'Back Throw -> Front'},
-		111: {msg: 'Back'}, //Big hammer back
-		112: {msg: 'Handle'},
-		113: {msg: 'Throw'},
-		114: {msg: 'Front Slam'},
-		115: {msg: 'Knockup'},//114 115
-		116: {msg: 'Donuts'},
-		117: {msg: 'Jump (Bait)'},
-		118: {msg: 'Jump (Tank)'},
-		119: {msg: 'Left swipe'},
-		120: {msg: 'Right swipe'},
-		121: {msg: 'Waves (Left)'},
-		122: {msg: 'Waves (Left) 3rd fast'},
-		123: {msg: 'Waves (Left) 2nd fast'},
-		125: {msg: 'Front -> Right Scratch'},//125 126 127
-		131: {msg: 'Front -> Left Scratch'},//131 132 134
+		101: {msg: 'Combo x4'},//101 105 107 139
+		103: {msg: 'Frente (Esquivar)'},//103 104
+//		105: {msg: '4 Golpes (2)'},
+	//		107: {msg: '4 Golpes (3)'},	
+		108: {msg: 'Tiro Atras -> Frente'},
+		111: {msg: 'Atras'}, //Big hammer back
+		112: {msg: 'Mango'},
+		113: {msg: 'Lanzar'},
+		114: {msg: 'Golpe Frontal'},
+		115: {msg: 'Pelotear'},//114 115
+		116: {msg: 'Circulos'},
+		117: {msg: 'Salto (Cebo)'},
+		118: {msg: 'Salto (Tank)'},
+		119: {msg: 'Izquierda (Deslizar)'},
+		120: {msg: 'Derecha (Deslizar)'},
+		121: {msg: 'Ondas (Izquierda)'},
+		122: {msg: 'Ondas (Izquierda) 3rd rapido'},
+		123: {msg: 'Ondas (Izquierda) 2nd rapido'},
+		125: {msg: 'Frente -> Rascar Derecho'},//125 126 127
+		131: {msg: 'Frente -> Rascar Izquierdo'},//131 132 134
 		135: {msg: 'Handle'},
 //		137: {msg: 'Hammer back '},
-		138: {msg: 'Knockup (Bait)'},//113 138		
-		139: {msg: 'Dodge!!'}, //108 110 139    118 139    101 105 107 139    (Knockdown)
-		140: {msg: 'Waves (Right)'},
-		141: {msg: 'Waves (Right) 3rd fast'},
-		142: {msg: 'Waves (Right) 2nd fast'},
-		305: {msg: 'Meteors!!'},
-		306: {msg: 'Meteors!!'},
-//		307: {msg: 'Bombs'},//307 301
+		138: {msg: 'Pelotear (Cebo)'},//113 138		
+		139: {msg: 'Esquivar!!'}, //108 110 139    118 139    101 105 107 139    (Knockdown)
+		140: {msg: 'Ondas (Derecha)'},
+		141: {msg: 'Ondas (Derecha) 3rd rapido'},
+		142: {msg: 'Ondas (Derecha) 2nd rapido'},
+		305: {msg: 'Meteorito!!'},
+		306: {msg: 'Meteorito!!'},
+//		307: {msg: 'Bombas'},//307 301
 //		308: {msg: 'Stun (1)?'},
 //		309: {msg: 'Stun (2)'},
 //		310: {msg: 'Stun (3)'},
-		311: {msg: 'Wrath'},//311 120    311 119
-		312: {msg: 'Wrath!'},//312 119    312 120
+		311: {msg: 'Ira'},//311 120    311 119
+		312: {msg: 'Ira!'},//312 119    312 120
+
 	}
 }
